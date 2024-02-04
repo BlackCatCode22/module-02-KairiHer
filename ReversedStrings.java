72d2636348b5407a2bdae2e32a958e4f62ca1eec
KairiHer <144934520+KairiHer@users.noreply.github.com>
2024-02-03 18:16:17 -0800

    Add files via upload
    My Take on the Reverse strings

--- ReversedStrings.java
+++ ReversedStrings.java
@@ -1,43 +1,34 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class ReversedStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt user for input
         System.out.print("Enter a string: ");
         String inputString = scanner.nextLine();
 
-        // Reverse the input string
         String reversedString = reverseString(inputString);
 
-        // Display the reversed string
         System.out.println("Reversed string: " + reversedString);
 
-        // Close the scanner
         scanner.close();
     }
 
-    // Method to reverse a string
+
     private static String reverseString(String input) {
         char[] charArray = input.toCharArray();
         int start = 0;
         int end = input.length() - 1;
 
         while (start < end) {
-            // Swap characters at start and end positions
             char temp = charArray[start];
             charArray[start] = charArray[end];
             charArray[end] = temp;
 
-            // Move towards the center of the string
             start++;
             end--;
         }
 
-        // Convert the char array back to a string
         return new String(charArray);
     }
 }
